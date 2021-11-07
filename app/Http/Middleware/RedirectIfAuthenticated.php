f84c47a44133281a2416029ba670052ae94ae105
kr95656 <katmsg8239@gmail.com>
2021-11-07 14:17:18 +0900

    ログイン後のページ遷移先の変更

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,9 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            // return redirect('top');
+            return redirect(RouteServiceProvider::TOP);
+            // return redirect(RouteServiceProvider::HOME);
         }
 
         return $next($request);
